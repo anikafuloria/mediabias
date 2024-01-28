70e27bc00ec33b8dba618b97d21a5ddd8995230d
Anika Fuloria <62360723+anikafuloria@users.noreply.github.com>
2024-01-28 12:35:33 -0800

    Updated to scrape the top 25 articles about Israel/Palestine

--- nytscrape.py
+++ nytscrape.py
@@ -1,6 +1,4 @@
 from pynytimes import NYTAPI
-
-# Import datetime function from datetime library which allows us to create a datetime object
 from datetime import datetime
 
 api_key = "XXXXXXXXXX" # make sure to X this out before committing code
@@ -12,28 +10,31 @@ begin = datetime(2023, 1, 1)
 
 end = datetime(2024, 1, 31)
 
-#Create dictionary containing dates data
-
+# Set the beginning and end dates for the search.
 date_dict = {"begin":begin, "end":end}
 
 nyt = NYTAPI(api_key, parse_dates=True)
 
-# Grab the first data item in top_stories and view it
-top_stories = nyt.top_stories()
-
-top_story = top_stories[0]
-
-print("The top story is: ", top_story['title'])
-
-# Retrieve the most viewed articles for today.
+file = open('readdata.txt', 'w')
 
 articles = nyt.article_search(query = "Israel Palestine", 
-                              results = 10,
+                              results = 25,
                               dates = date_dict)
 
-# Assign the data in the first item of articles to a variable
-article = articles[0].copy()
+for article in articles:
+    weburl = article["web_url"]
+    abstract = article["abstract"]
 
-print(article["web_url"])
-print(article["abstract"])
-print(article["multimedia"])
+    file.write(weburl + "\n")
+    file.write(abstract + "\n")
+    
+    multimedia = article["multimedia"]
+    for m in multimedia:
+        mediatype = m["type"]
+        caption = m["caption"]
+        url = "https://www.nytimes.com/" + m["url"]
+
+        if mediatype == "image" and caption != None:
+            file.write(url + "\n")
+            file.write(caption + "\n")
+        file.write("\n")
